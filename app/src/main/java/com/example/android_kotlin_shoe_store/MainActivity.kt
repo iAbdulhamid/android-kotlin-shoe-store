13c145b63e5915a94975f08d60c8a7aae720f62b
Ibrahim Abdelhamed <i_abdulhamid@outlook.com>
2023-04-08 14:52:50 +0200

    Adding dataBinding implementation to MainActivity

--- app/src/main/java/com/example/android_kotlin_shoe_store/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_shoe_store/MainActivity.kt
@@ -2,12 +2,26 @@ package com.example.android_kotlin_shoe_store
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.android_kotlin_shoe_store.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
+    }
 
 }
